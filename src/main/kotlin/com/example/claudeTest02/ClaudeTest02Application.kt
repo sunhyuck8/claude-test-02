906eac6647afd2c5bece74c57be4b5b025eba647
SunH8 <awfullysh82@gmail.com>
2025-05-05 19:53:20 +0900

    Add main application file

---
+++ src/main/kotlin/com/example/claudeTest02/ClaudeTest02Application.kt
@@ -0,0 +1,11 @@
+package com.example.claudeTest02
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ClaudeTest02Application
+
+fun main(args: Array<String>) {
+	runApplication<ClaudeTest02Application>(*args)
+}
