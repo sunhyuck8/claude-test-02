146896347c30912f28c50310cbdeb0c2fad09a5f
SunH8 <awfullysh82@gmail.com>
2025-05-05 19:53:30 +0900

    Add application test file

---
+++ src/test/kotlin/com/example/claudeTest02/ClaudeTest02ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.claudeTest02
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ClaudeTest02ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
